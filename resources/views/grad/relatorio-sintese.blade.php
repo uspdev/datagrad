e385d6314ac1bc23f20169b4ef786aa28a048faa
Masaki K Neto <kawabata@usp.br>
2023-04-24 10:12:43 -0300

    renomeando arquivo

--- resources/views/grad/relatorio-sintese.blade.php
+++ resources/views/grad/relatorio-sintese.blade.php
@@ -2,22 +2,7 @@
 
 @section('content')
 
-  <form method="POST" action="graduacao/relatorio/nomes">
-    @csrf
-    <div class="form-group">
-      <label for="exampleFormControlTextarea1" class="h4">Nomes (1 por linha)</label>
-      <button type="submit" class="btn btn-sm btn-primary spinner">Enviar</button>
-      <textarea name="nomes" class="form-control" id="exampleFormControlTextarea1" rows="4">{{ $nomes }}</textarea>
-    </div>
-  </form>
-
-  @if ($naoEncontrados)
-    <hr>
-    <div class="h4">Não encontrados</div>
-    @foreach ($naoEncontrados as $nome)
-      {{ $nome }}<br>
-    @endforeach
-  @endif
+  @include('grad.partials.relatorio-form')
 
   @if ($pessoas)
     <hr>
