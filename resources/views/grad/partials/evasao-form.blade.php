2d2b226791b94e8da941efb3a15dd6b4d02a9ef4
Vinicios Isael <viisael97@usp.br>
2023-10-17 13:08:02 -0300

    Mudanças no view e partials evasao

--- resources/views/grad/partials/evasao-form.blade.php
+++ resources/views/grad/partials/evasao-form.blade.php
@@ -1,85 +1,32 @@
-@isset($cursoOpcao)
-
-    <h4>Relatório de Evasão</h4> <br>
-
-    <form action="{{ route('graduacao.relatorio.evasao.post') }}" method="POST" class="form-group">
-
-        @csrf
-
-        <label for="tipo">Calcular para:</label>
-        <select id="tipo" name="tipo">
-
-            <option value='' {{ old('tipo') == '' ? 'selected' : 'selected' }}></option>
-            <option value='curso' {{ old('tipo') == 'curso' ? 'selected' : '' }}>Curso</option>
-            <option value='unidade' {{ old('tipo') == 'unidade' ? 'selected' : '' }}>Unidade</option>
-
-
-        </select>
-
-        <br>
-
-        <div id=opcaoCursos style='visibility: hidden'>
-
-
-            <label for="curso">Curso:</label>
-            <select id="curso" name="curso">
-
-                <option value=''></option>
-
-                @foreach ($cursoOpcao as $curso)
-                    <option value={{ $curso['cod'] }} {{ old('curso') == $curso['cod'] ? 'selected' : '' }}>
-                        {{ $curso['cod'] }} - {{ $curso['nome'] }} </option>
-                @endforeach
-
-            </select>
-
-        </div>
-
-        <label for="anoIngresso">Ano de ingresso da turma: </label>
-        <select id="anoIngresso" name="ano">
-
-            <option value=''></option>
-
-            @foreach (range(2015, date('Y') - 1) as $ano)
-                <option value={{ $ano }} {{ old('ano') == $ano ? 'selected' : '' }}>{{ $ano }}</option>
-            @endforeach
-
-        </select>
-
-        <br><br>
-
-        <button type="submit" class="btn btn-sm btn-primary spinner">Cálcular</button>
-    </form>
-
-    <script>
-        var opcaoTipo = document.getElementById('tipo');
-        var opcaoCurso = document.getElementById('opcaoCursos');
-
-        opcaoTipo.addEventListener('input', function() {
-            var opcaoSelecionada = opcaoTipo.value;
-
-
-            if (opcaoSelecionada === 'unidade' || opcaoSelecionada === '') {
-
-                opcaoCurso.style.visibility = 'hidden'
-
-            } else {
-
-                opcaoCurso.style.visibility = 'visible'
-            }
-        })
-
-        function verificarOpcaoTipo() {
-
-            if (opcaoTipo.value == 'curso') {
-
-                opcaoCurso.style.visibility = 'visible'
-
-            }
-
-        }
-
-        verificarOpcaoTipo()
-    </script>
-
-@endisset
+<form action="{{ route('graduacao.relatorio.evasao.post') }}" method="POST" class="form-group">
+  @csrf
+  <div id=opcaoCursos>
+    <label for="curso">Curso:</label>
+    <select id="curso" name="curso">
+      <option value=''>Todos os cursos</option>
+      @foreach ($cursoOpcao as $curso)
+        @if (isset($formRequest))
+          <option value={{ $curso['codcur'] }} {{ $formRequest['codcur'] == $curso['codcur'] ? 'selected' : '' }}>
+            {{ $curso['codcur'] }} - {{ $curso['nomcur'] }} </option>
+        @else
+          <option value={{ $curso['codcur'] }} {{ old('curso') == $curso['codcur'] ? 'selected' : '' }}>
+            {{ $curso['codcur'] }} - {{ $curso['nomcur'] }} </option>
+        @endif
+      @endforeach
+    </select>
+  </div>
+  <label for="anoIngresso">Ano de ingresso da turma: </label>
+  <select id="anoIngresso" name="ano">
+    <option value=''></option>
+    @foreach (range(2015, date('Y') - 1) as $ano)
+      @if (isset($formRequest))
+        <option value={{ $ano }} {{ $formRequest['anoIngresso'] == $ano ? 'selected' : '' }}>
+          {{ $ano }}</option>
+      @else
+        <option value={{ $ano }} {{ old('ano') == $ano ? 'selected' : '' }}>{{ $ano }}</option>
+      @endif
+    @endforeach
+  </select>
+  <br><br>
+  <button type="submit" class="btn btn-sm btn-primary spinner">Calcular</button>
+</form>
--- resources/views/grad/relatorio-evasao.blade.php
+++ resources/views/grad/relatorio-evasao.blade.php
@@ -3,44 +3,43 @@
 
 @section('content')
 
+  @isset($formRequest)
+    <h4>
+      <a href="{{ route('graduacao.relatorio.evasao') }}">Relatório de Evasão</a>
+      <i class="fas fa-angle-right"></i> {{ $formRequest['codcur'] }} - {{ $formRequest['nomcur'] }}
+    </h4>
+  @else
+    <h4>
+      @if (isset($taxaEvasao))
+        <a href="{{ route('graduacao.relatorio.evasao') }}">Relatório de Evasão</a>
+        <i class="fas fa-angle-right"></i> Todos os Cursos
+      @else
+        Relatório de Evasão
+      @endif
+    </h4>
+  @endisset
 
 
-    @include('grad.partials.evasao-form')
 
-    @isset($taxaEvasao)
-        @isset($cursoRequest)
-            <h4>
+  @include('grad.partials.evasao-form')
 
-                <a href="{{ route('graduacao.relatorio.evasao') }}">Relatório de Evasão</a>
-                <i class="fas fa-angle-right"></i> {{ $cursoRequest['cod'] }} - {{ $cursoRequest['nome'] }}
-
-            </h4>
-        @else
-            <h4>
-
-                <a href="{{ route('graduacao.relatorio.evasao') }}">Relatório de Evasão</a>
-                <i class="fas fa-angle-right"></i> Unidade 18
-
-            </h4>
-        @endisset
-
-        <br>
-
-        <table class="table table-bordered table-hover">
-            <tr>
-                <th>Ano</th>
-                <th>Permanência (%)</th>
-                <th>Desistência (%)</th>
-                <th>Conclusão (%)</th>
-            </tr>
-            @foreach ($taxaEvasao as $ano => $taxa)
-                <tr>
-                    <td>{{ $ano }}</td>
-                    <td> {{ $taxa['txPermanencia'] }}</td>
-                    <td> {{ $taxa['txDesistenciaAcc'] }}</td>
-                    <td> {{ $taxa['txConclusaoAcc'] }}</td>
-                </tr>
-            @endforeach
-        </table>
-    @endisset
+  @isset($taxaEvasao)
+    <br>
+    <table class="table table-bordered table-hover">
+      <tr>
+        <th>Ano</th>
+        <th>Permanência (%)</th>
+        <th>Desistência (%)</th>
+        <th>Conclusão (%)</th>
+      </tr>
+      @foreach ($taxaEvasao as $ano => $taxa)
+        <tr>
+          <td>{{ $ano }}</td>
+          <td> {{ $taxa['txPermanencia'] }}</td>
+          <td> {{ $taxa['txDesistenciaAcc'] }}</td>
+          <td> {{ $taxa['txConclusaoAcc'] }}</td>
+        </tr>
+      @endforeach
+    </table>
+  @endisset
 @endsection
