3f253f1b158d2d653277d083af37788e2a97fa7b
Masaki Kawabata Neto <kawabata@usp.br>
2024-05-17 14:41:59 -0300

    roles wip

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -24,8 +24,9 @@
 
 @section('content')
   <div class="navbar navbar-light card-header-sticky justify-content-between mb-3 pb-1">
-    <div class="h5">
-      Funções
+    <div>
+      <span class="h5">Funções</span>
+      <i class="fas fa-angle-right"></i> Esta interface permite gerenciar as funções dos usuários dentro do sistema.
     </div>
     <div class="form-inline">
     </div>
@@ -36,41 +37,41 @@
       <form method="post" action="{{ route('roles.update', 'cg') }}">
         @csrf
         @method('put')
-        <div class="h5">
-          Comissão de graduação (CG)
-          @include('disciplinas.partials.codpes-adicionar-btn')
-        </div>
-        <div class="ml-3">
-          @foreach ($roleCG->users->sortBy('name') as $user)
-            <div class="hover"><span>{{ $user->name }}</span>
-              <span class="hide">
-                @include('disciplinas.partials.codpes-remover-btn', ['codpes' => $user->codpes])
-              </span>
-            </div>
-          @endforeach
+        <div>
+          <div class="h5">
+            Comissão de graduação (CG)
+            @include('disciplinas.partials.codpes-adicionar-btn')
+          </div>
+          <div class="ml-3">
+            @foreach ($roleCG->users->sortBy('name') as $user)
+              <div class="hover"><span>{{ $user->name }}</span>
+                <span class="hide">
+                  @include('disciplinas.partials.codpes-remover-btn', ['codpes' => $user->codpes])
+                </span>
+              </div>
+            @endforeach
+          </div>
         </div>
       </form>
 
       <br>
-      <div class="h5">Departamentos</div>
-      @foreach ($departamentos as $role)
-        <b>{{ $role->name }}</b><br>
-        @foreach ($role->users as $user)
-          {{ $user->name }} - {{ $user->permissions->pluck('name') }}<br>
-        @endforeach
-      @endforeach
-
-      <br>
+      <div class="h5">Coordenadores de cursos</div>
 
     </div>
     <div class="col-md-6">
-      <div class="h5">Cursos</div>
-      {{-- @foreach ($roles as $role)
-    <b>{{ $role->name }}</b><br>
-    @foreach ($role->users as $user)
-      {{ $user->name }}<br>
-    @endforeach
-  @endforeach --}}
+
+      <div class="h5">Grupos de disciplinas / Chefes de departamentos</div>
+      @foreach ($departamentos as $role)
+        <b>{{ $role->name }}</b><br>
+        <div class="ml-3">
+          @foreach ($role->users as $user)
+            {{ $user->name }}
+            {{-- - {{ $user->permissions->pluck('name') }} --}}
+            <br>
+          @endforeach
+        </div>
+      @endforeach
+
     </div>
   </div>
 @endsection
