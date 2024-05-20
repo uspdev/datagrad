495495f4adf5f51beaf46a037676f5bd2fc9f520
Masaki Kawabata Neto <kawabata@usp.br>
2024-05-20 14:26:55 -0300

    colocando hover

--- resources/views/disciplinas/partials/form-responsaveis.blade.php
+++ resources/views/disciplinas/partials/form-responsaveis.blade.php
@@ -2,22 +2,32 @@
   Responsáveis/Professors
   @include('disciplinas.partials.codpes-adicionar-btn')
 </div>
+
 <div class="ml-2 font-weight-bold">
   @foreach ($disc->responsaveis as $r)
     @if ($r['status'] == 'mesmo')
-      <div>
+      <div class="hover">
         {{ $r['codpes'] }} - {{ $r['nompesttd'] }}
         @include('disciplinas.partials.codpes-remover-btn', ['codpes' => $r['codpes']])
       </div>
     @elseif($r['status'] == 'novo')
-      <div>
+      <div class="hover">
         <ins class="ins">{{ $r['codpes'] }} - {{ $r['nompesttd'] }}</ins>
         @include('disciplinas.partials.codpes-remover-btn', ['codpes' => $r['codpes']])
       </div>
     @elseif($r['status'] == 'removido')
-      <div class="diff d-none">
+      <div class="diff d-none hover">
         <del>{{ $r['codpes'] }} - {{ $r['nompesttd'] }}</del>
       </div>
     @endif
   @endforeach
 </div>
+
+@section('styles')
+  @parent
+  <style>
+    .hover:hover {
+      background-color: gainsboro;
+    }
+  </style>
+@endsection
--- resources/views/disciplinas/partials/navbar-edit.blade.php
+++ resources/views/disciplinas/partials/navbar-edit.blade.php
@@ -13,10 +13,6 @@
 
   <div class="">
     @if (Request::is('*/edit'))
-      <div class="btn btn-sm btn-info" data-toggle="popover"
-        title="Última alteração em {{ $disc->updated_at->format('d/m/Y') }} por {{ $disc->atualizadoPor->name }}">
-        <i class="fas fa-info-circle"></i>
-      </div>
       <a href="{{ route('disciplinas.show', $disc->coddis) }}" class="btn btn-sm btn-secondary ml-2">
         Cancelar
       </a>
@@ -32,7 +28,6 @@
       <a href="{{ route('disciplinas.edit', $disc['coddis']) }}" class="btn btn-sm btn-primary ml-2" type="submit">
         Voltar para edição
       </a>
-
       <button class="btn btn-sm btn-success ml-2" type="submit" name="submit" value="preview">
         Enviar para avaliação
       </button>
