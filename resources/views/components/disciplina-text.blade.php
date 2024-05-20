bc0623b2c3ef8b52af8971dab383a8fc04e3ba66
Masaki Kawabata Neto <kawabata@usp.br>
2024-05-20 14:27:05 -0300

    testes wip

--- resources/views/components/disciplina-text.blade.php
+++ resources/views/components/disciplina-text.blade.php
@@ -4,6 +4,17 @@
     'model' => '',
 ])
 
+{{-- <div class="mb-3 {{ $class }}">
+  <div class="input-group input-group-sm">
+    <span class="mr-2">{{ $model->meta[$name]['titulo'] }}</span>
+    <div class="input-group-prepend diff d-none">
+      <span class="input-group-text px-3">{{ $model->dr[$name] }}</span>
+    </div>
+    <input type="text" name="{{ $name }}" value="{{ $model->{$name} }}" data-original="{!! $model->dr[$name] !!}"
+      class="form-control">
+  </div>
+</div> --}}
+
 <table class="table table-bordered table-sm {{ $model->meta[$name]['class'] ?? '' }}">
   <tr>
     <th colspan="2" class="titulo text-center" style="background-color: aliceBlue">
