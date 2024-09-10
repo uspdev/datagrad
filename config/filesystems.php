800eff0ecf6703ec25a7919b07184d8e7e79a5e6
Masaki Kawabata Neto <kawabata@usp.br>
2024-09-10 07:55:33 -0300

    instalando db snapshot

--- config/filesystems.php
+++ config/filesystems.php
@@ -35,6 +35,11 @@ return [
             'root' => storage_path('app'),
         ],
 
+        'snapshots' => [
+            'driver' => 'local',
+            'root' => storage_path('snapshots'),
+        ],
+        
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
