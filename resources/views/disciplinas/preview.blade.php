672dedc9be2d7e367a7366bb1b8545801c997552
Masaki Kawabata Neto <kawabata@usp.br>
2025-02-13 07:42:41 -0300

    melhorando download para firefox

--- resources/views/disciplinas/preview.blade.php
+++ resources/views/disciplinas/preview.blade.php
@@ -17,13 +17,15 @@
 @endsection
 
 @section('content')
-    @include('disciplinas.partials.navbar-preview')
+  @include('disciplinas.partials.navbar-preview')
 
   <div class="row">
     <div class="col-12">
-      <object class="pdf" type="application/pdf" data="{{ $url }}" height="800">
+      <embed src="{{ $url }}" type="application/pdf" width="100%" height="800px">
+      <a href="{{ $url }}" target="_blank">Abrir PDF</a>
+      {{-- <object class="pdf" type="application/pdf" data="{{ $url }}" height="800">
       Alt: <a href="disciplina.pdf">disciplina.pdf</a>
-      </object>
+      </object> --}}
     </div>
   </div>
 @endsection
