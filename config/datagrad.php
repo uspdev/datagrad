28394d9e63315ada9f96321803287d6eb801c851
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2025-06-05 10:39:12 -0300

    Adicionando códigos de unidade no config

--- config/datagrad.php
+++ config/datagrad.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-
+    'codundclgs' => explode(',', env('REPLICADO_CODUNDCLGS', null)),
     'evasaoCodcurIgnorados' => explode(',', env('EVASAO_CODCUR_IGNORADOS', null)),
     'evasaoCodcurHabIgnorados' => explode(',', env('EVASAO_CODCUR_HAB_IGNORADOS', null)),
 ];
