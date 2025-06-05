56f9d8d1d9351499bf4553bacc7deb60b5716344
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2025-06-05 11:19:24 -0300

    Considerando habilitações além de codhab = 0

--- config/datagrad.php
+++ config/datagrad.php
@@ -4,4 +4,5 @@ return [
 
     'evasaoCodcurIgnorados' => explode(',', env('EVASAO_CODCUR_IGNORADOS', null)),
     'evasaoCodcurHabIgnorados' => explode(',', env('EVASAO_CODCUR_HAB_IGNORADOS', null)),
+    'codhabs' => explode(',', env('CODHABS', null)),
 ];
