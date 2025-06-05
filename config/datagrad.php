c94a2d18e3646720e14269802817786c36a3cc75
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2025-06-05 13:21:20 -0300

    Informando quais habilitações são consideradas

--- config/datagrad.php
+++ config/datagrad.php
@@ -4,5 +4,5 @@ return [
 
     'evasaoCodcurIgnorados' => explode(',', env('EVASAO_CODCUR_IGNORADOS', null)),
     'evasaoCodcurHabIgnorados' => explode(',', env('EVASAO_CODCUR_HAB_IGNORADOS', null)),
-    'codhabs' => explode(',', env('CODHABS', null)),
+    'codhabs' => explode(',', env('CODHABS', 0)),
 ];
