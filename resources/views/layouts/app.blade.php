f34d7aae46e10fdb084f735337a5926e68f0a7cc
Masaki Kawabata Neto <kawabata@usp.br>
2026-03-30 17:19:12 -0300

    fix: mostrando emnsagens de erro

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,18 @@
 @include('blocos.textarea-autoexpand')
 
 @section('flash')
-  <div class="flash-message fixed-top w-50 ml-auto mr-auto" style="margin-top: 60px">
+
+  @if ($errors->any())
+    <div class="alert alert-danger">
+      <ul>
+        @foreach ($errors->all() as $error)
+          <li>{!! $error !!}</li>
+        @endforeach
+      </ul>
+    </div>
+  @endif
+
+  <div class="flash-message fixed-top w-50 ml-auto mr-auto" style="margin-top: 60px; z-index: 9999;">
     @foreach (['danger', 'warning', 'success', 'info'] as $msg)
       @if (Session::has('alert-' . $msg))
         <p class="alert alert-{{ $msg }} border border-dark rounded">{{ Session::get('alert-' . $msg) }}
@@ -28,6 +39,7 @@
     @endforeach
   </div>
 @endsection
+
 @section('javascripts_bottom')
   @parent
   <script>
